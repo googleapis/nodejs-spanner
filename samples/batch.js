afe35cd66352a08eeeb3408f32c268e5058bbf47
Alexander Fenster <github@fenster.name>
2018-01-22 08:12:03 -0800

    feature: batch queries

---
+++ samples/batch.js
@@ -0,0 +1,246 @@
+/**
+ * Copyright 2018, Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+function createBatchTransaction(instanceId, databaseId, projectId) {
+  // [START create_batch_transaction]
+  // Imports the Google Cloud client library
+  const Spanner = require('@google-cloud/spanner');
+
+  /**
+   * TODO(developer): Uncomment the following lines before running the sample.
+   */
+  // const projectId = 'my-project-id';
+  // const instanceId = 'my-instance';
+  // const databaseId = 'my-database';
+
+  // Creates a client
+  const spanner = new Spanner({
+    projectId: projectId,
+  });
+
+  // Gets a reference to a Cloud Spanner instance and database
+  const instance = spanner.instance(instanceId);
+  const database = instance.database(databaseId);
+
+  database
+    .createBatchTransaction()
+    .then(data => {
+      const transaction = data[0];
+
+      console.log(`Created batch transaction for ${databaseId}`);
+
+      // Close the transaction when finished.
+      return transaction.close();
+    })
+    .then(
+      () => {
+        console.log(`Closed batch transaction`);
+      },
+      err => {
+        console.error('ERROR:', err);
+      }
+    );
+  // [END create_batch_transaction]
+}
+
+function createQueryPartitions(instanceId, databaseId, identifier, projectId) {
+  // [START create_query_partitions]
+  // Imports the Google Cloud client library
+  const Spanner = require('@google-cloud/spanner');
+
+  /**
+   * TODO(developer): Uncomment the following lines before running the sample.
+   */
+  // const projectId = 'my-project-id';
+  // const instanceId = 'my-instance';
+  // const databaseId = 'my-database';
+  // const identifier = {};
+
+  // Creates a client
+  const spanner = new Spanner({
+    projectId: projectId,
+  });
+
+  // Gets a reference to a Cloud Spanner instance and database
+  const instance = spanner.instance(instanceId);
+  const database = instance.database(databaseId);
+  const transaction = database.batchTransaction(identifier);
+
+  const query = 'SELECT * FROM Singers';
+
+  transaction
+    .createQueryPartitions(query)
+    .then(data => {
+      const partitions = data[0];
+      console.log(`Successfully created query partitions.`);
+    })
+    .catch(err => {
+      console.error('ERROR:', err);
+    });
+  // [END create_query_partitions]
+}
+
+function createReadPartitions(instanceId, databaseId, identifier, projectId) {
+  // [START create_read_partitions]
+  // Imports the Google Cloud client library
+  const Spanner = require('@google-cloud/spanner');
+
+  /**
+   * TODO(developer): Uncomment the following lines before running the sample.
+   */
+  // const projectId = 'my-project-id';
+  // const instanceId = 'my-instance';
+  // const databaseId = 'my-database';
+  // const identifier = {};
+
+  // Creates a client
+  const spanner = new Spanner({
+    projectId: projectId,
+  });
+
+  // Gets a reference to a Cloud Spanner instance and database
+  const instance = spanner.instance(instanceId);
+  const database = instance.database(databaseId);
+  const transaction = database.batchTransaction(identifier);
+
+  const options = {
+    table: 'Singers',
+    keys: ['1'],
+    columns: ['SingerId'],
+  };
+
+  transaction
+    .createReadPartitions(options)
+    .then(data => {
+      const partitions = data[0];
+      console.log(`Successfully created read partitions.`);
+    })
+    .catch(err => {
+      console.error('ERROR:', err);
+    });
+  // [END create_read_partitions]
+}
+
+function executePartition(
+  instanceId,
+  databaseId,
+  identifier,
+  partition,
+  projectId
+) {
+  // [START execute_partition]
+  // Imports the Google Cloud client library
+  const Spanner = require('@google-cloud/spanner');
+
+  /**
+   * TODO(developer): Uncomment the following lines before running the sample.
+   */
+  // const projectId = 'my-project-id';
+  // const instanceId = 'my-instance';
+  // const databaseId = 'my-database';
+  // const identifier = {};
+  // const partition = {};
+
+  // Creates a client
+  const spanner = new Spanner({
+    projectId: projectId,
+  });
+
+  // Gets a reference to a Cloud Spanner instance and database
+  const instance = spanner.instance(instanceId);
+  const database = instance.database(databaseId);
+  const transaction = database.batchTransaction(identifier);
+
+  transaction
+    .execute(partition)
+    .then(data => {
+      const rows = data[0];
+      console.log(`Successfully executed partition.`);
+    })
+    .catch(err => {
+      console.error('ERROR:', err);
+    });
+  // [END execute_partition]
+}
+
+require(`yargs`)
+  .demand(1)
+  .command(
+    `create-batch-transaction <instanceName> <databaseName> <projectId>`,
+    'Creates a batch transaction for an example Cloud Spanner database.',
+    {},
+    opts =>
+      createBatchTransaction(
+        opts.instanceName,
+        opts.databaseName,
+        opts.projectId
+      )
+  )
+  .command(
+    `create-query-partitions <instanceName> <databaseName> <identifier> <projectId>`,
+    'Creates query partitions.',
+    {},
+    opts =>
+      createQueryPartitions(
+        opts.instanceName,
+        opts.databaseName,
+        JSON.parse(opts.identifier),
+        opts.projectId
+      )
+  )
+  .command(
+    `create-read-partitions <instanceName> <databaseName> <identifier> <projectId>`,
+    'Creates read partitions.',
+    {},
+    opts =>
+      createReadPartitions(
+        opts.instanceName,
+        opts.databaseName,
+        JSON.parse(opts.identifier),
+        opts.projectId
+      )
+  )
+  .command(
+    `execute-partition <instanceName> <databaseName> <identifier> <partition> <projectId>`,
+    'Executes a partition.',
+    {},
+    opts =>
+      executePartition(
+        opts.instanceName,
+        opts.databaseName,
+        JSON.parse(opts.identifier),
+        JSON.parse(opts.partition),
+        opts.projectId
+      )
+  )
+  .example(
+    `node $0 create-batch-transaction "my-instance" "my-database" "{}" "my-project-id"`
+  )
+  .example(
+    `node $0 create-query-partitions "my-instance" "my-database" "{}" "my-project-id"`
+  )
+  .example(
+    `node $0 create-read-partitions "my-instance" "my-database" "{}" "my-project-id"`
+  )
+  .example(
+    `node $0 execute-partition "my-instance" "my-database" "{}" "{}" "my-project-id"`
+  )
+  .wrap(120)
+  .recommendCommands()
+  .epilogue(`For more information, see https://cloud.google.com/spanner/docs`)
+  .strict()
+  .help().argv;
---
+++ test/batch-transaction.js
@@ -0,0 +1,336 @@
+/*!
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var assert = require('assert');
+var extend = require('extend');
+var proxyquire = require('proxyquire');
+var util = require('@google-cloud/common').util;
+
+var promisified = false;
+var fakeUtil = extend({}, util, {
+  promisifyAll: function(Class, options) {
+    if (Class.name !== 'BatchTransaction') {
+      return;
+    }
+
+    assert.deepEqual(options.exclude, ['identifier']);
+    promisified = true;
+  },
+});
+
+var fakeCodec = {
+  encode: util.noop,
+  Int: function() {},
+  Float: function() {},
+  SpannerDate: function() {},
+};
+
+function FakeTransaction(session) {
+  this.calledWith_ = arguments;
+  this.session = session;
+}
+
+describe('BatchTransaction', function() {
+  var BatchTransaction;
+  var batchTransaction;
+
+  var SESSION = {};
+
+  before(function() {
+    BatchTransaction = proxyquire('../src/batch-transaction.js', {
+      '@google-cloud/common': {
+        util: fakeUtil,
+      },
+      './codec.js': fakeCodec,
+      './transaction.js': FakeTransaction,
+    });
+  });
+
+  beforeEach(function() {
+    batchTransaction = new BatchTransaction(SESSION);
+  });
+
+  describe('instantiation', function() {
+    it('should promisify all the things', function() {
+      assert(promisified);
+    });
+
+    it('should extend the Transaction class', function() {
+      var batchTransaction = new BatchTransaction(SESSION);
+
+      assert(batchTransaction instanceof FakeTransaction);
+      assert.strictEqual(batchTransaction.calledWith_[0], SESSION);
+      assert.deepEqual(batchTransaction.calledWith_[1], {readOnly: true});
+    });
+  });
+
+  describe('close', function() {
+    it('should delete the session', function(done) {
+      SESSION.delete = function(callback) {
+        callback(); // the done fn
+      };
+
+      batchTransaction.close(done);
+    });
+  });
+
+  describe('createQueryPartitions', function() {
+    var GAX_OPTS = {a: 'b'};
+    var QUERY = {
+      sql: 'SELECT * FROM Singers',
+      gaxOptions: GAX_OPTS,
+    };
+
+    it('should make the correct request', function(done) {
+      fakeCodec.encodeQuery = function(query) {
+        assert.deepEqual(query, {sql: QUERY.sql});
+        return QUERY;
+      };
+
+      batchTransaction.createPartitions_ = function(config, callback) {
+        assert.strictEqual(config.client, 'SpannerClient');
+        assert.strictEqual(config.method, 'partitionQuery');
+        assert.strictEqual(config.reqOpts, QUERY);
+        callback(); // the done fn
+      };
+
+      batchTransaction.createQueryPartitions(QUERY.sql, done);
+    });
+
+    it('should remove gax options from the query', function(done) {
+      var fakeQuery = {
+        sql: QUERY.sql,
+        gaxOptions: GAX_OPTS,
+      };
+
+      fakeCodec.encodeQuery = function(query) {
+        assert.strictEqual(query, fakeQuery);
+        return extend({a: 'b'}, QUERY);
+      };
+
+      batchTransaction.createPartitions_ = function(config, callback) {
+        assert.deepEqual(config.reqOpts, {sql: QUERY.sql, a: 'b'});
+        assert.strictEqual(config.gaxOpts, GAX_OPTS);
+        callback(); // the done fn
+      };
+
+      batchTransaction.createQueryPartitions(fakeQuery, done);
+    });
+  });
+
+  describe('createPartitions_', function() {
+    var SESSION = {formattedName_: 'abcdef'};
+    var ID = 'ghijkl';
+    var TIMESTAMP = {seconds: 0, nanos: 0};
+
+    var PARTITIONS = [{partitionToken: 'a'}, {partitionToken: 'b'}];
+    var RESPONSE = {partitions: PARTITIONS};
+
+    var QUERY = {a: 'b'};
+    var CONFIG = {reqOpts: QUERY};
+
+    beforeEach(function() {
+      batchTransaction.session = SESSION;
+      batchTransaction.id = ID;
+
+      batchTransaction.request = function(config, callback) {
+        callback(null, RESPONSE);
+      };
+    });
+
+    it('should insert the session and transaction ids', function(done) {
+      batchTransaction.request = function(config) {
+        assert.strictEqual(config.reqOpts.a, 'b');
+        assert.strictEqual(config.reqOpts.session, SESSION.formattedName_);
+        assert.deepEqual(config.reqOpts.transaction, {id: ID});
+        done();
+      };
+
+      batchTransaction.createPartitions_(CONFIG, assert.ifError);
+    });
+
+    it('should return any request errors', function(done) {
+      var error = new Error('err');
+      var response = {};
+
+      batchTransaction.request = function(config, callback) {
+        callback(error, response);
+      };
+
+      batchTransaction.createPartitions_(CONFIG, function(err, parts, resp) {
+        assert.strictEqual(err, error);
+        assert.strictEqual(parts, null);
+        assert.strictEqual(resp, response);
+        done();
+      });
+    });
+
+    it('should return the prepared partition configs', function(done) {
+      var expectedQuery = {
+        a: 'b',
+        session: SESSION.formattedName_,
+        transaction: {id: ID},
+      };
+
+      batchTransaction.createPartitions_(CONFIG, function(err, parts) {
+        assert.ifError(err);
+
+        parts.forEach(function(partition, i) {
+          var expectedPartition = extend({}, expectedQuery, PARTITIONS[i]);
+          assert.deepEqual(partition, expectedPartition);
+        });
+
+        done();
+      });
+    });
+
+    it('should update the transaction with returned metadata', function(done) {
+      var response = extend({}, RESPONSE, {
+        transaction: {
+          id: ID,
+          readTimestamp: TIMESTAMP,
+        },
+      });
+
+      batchTransaction.request = function(config, callback) {
+        callback(null, response);
+      };
+
+      batchTransaction.createPartitions_(CONFIG, function(err, parts, resp) {
+        assert.strictEqual(resp, response);
+        assert.strictEqual(batchTransaction.id, ID);
+        assert.strictEqual(batchTransaction.readTimestamp, TIMESTAMP);
+        done();
+      });
+    });
+  });
+
+  describe('createReadPartitions', function() {
+    var GAX_OPTS = {};
+    var QUERY = {table: 'abc', gaxOptions: GAX_OPTS};
+
+    it('should make the correct request', function(done) {
+      var query = {};
+
+      fakeCodec.encodeRead = function(options) {
+        assert.strictEqual(options, query);
+        return QUERY;
+      };
+
+      batchTransaction.createPartitions_ = function(config, callback) {
+        assert.strictEqual(config.client, 'SpannerClient');
+        assert.strictEqual(config.method, 'partitionRead');
+        assert.strictEqual(config.reqOpts, QUERY);
+        callback(); // the done fn
+      };
+
+      batchTransaction.createReadPartitions(query, done);
+    });
+
+    it('should remove gax options from the query', function(done) {
+      var query = {gaxOptions: GAX_OPTS};
+
+      fakeCodec.encodeRead = function() {
+        return extend({}, QUERY);
+      };
+
+      batchTransaction.createPartitions_ = function(config, callback) {
+        assert.deepEqual(config.reqOpts, {table: QUERY.table});
+        assert.strictEqual(config.gaxOpts, GAX_OPTS);
+        callback(); // the done fn
+      };
+
+      batchTransaction.createReadPartitions(query, done);
+    });
+  });
+
+  describe('execute', function() {
+    it('should make read requests for read partitions', function(done) {
+      var partition = {table: 'abc'};
+
+      batchTransaction.read = function(table, options, callback) {
+        assert.strictEqual(table, partition.table);
+        assert.strictEqual(options, partition);
+        callback(); // the done fn
+      };
+
+      batchTransaction.execute(partition, done);
+    });
+
+    it('should make query requests for non-read partitions', function(done) {
+      var partition = {sql: 'SELECT * FROM Singers'};
+
+      batchTransaction.run = function(query, callback) {
+        assert.strictEqual(query, partition);
+        callback(); // the done fn
+      };
+
+      batchTransaction.execute(partition, done);
+    });
+  });
+
+  describe('executeStream', function() {
+    var STREAM = {};
+
+    it('should make read streams for read partitions', function() {
+      var partition = {table: 'abc'};
+
+      batchTransaction.createReadStream = function(table, options) {
+        assert.strictEqual(table, partition.table);
+        assert.strictEqual(options, partition);
+        return STREAM;
+      };
+
+      var stream = batchTransaction.executeStream(partition);
+
+      assert.strictEqual(stream, STREAM);
+    });
+
+    it('should make query streams for query partitions', function() {
+      var partition = {sql: 'SELECT * FROM Singers'};
+
+      batchTransaction.runStream = function(query) {
+        assert.strictEqual(query, partition);
+        return STREAM;
+      };
+
+      var stream = batchTransaction.executeStream(partition);
+
+      assert.strictEqual(stream, STREAM);
+    });
+  });
+
+  describe('identifier', function() {
+    var ID = Buffer.from('abc');
+    var SESSION = {id: 'def'};
+    var TIMESTAMP = {seconds: 0, nanos: 0};
+
+    beforeEach(function() {
+      batchTransaction.id = ID;
+      batchTransaction.session = SESSION;
+      batchTransaction.readTimestamp = TIMESTAMP;
+    });
+
+    it('should create a transaction identifier', function() {
+      var expectedId = ID.toString('base64');
+      var identifier = batchTransaction.identifier();
+
+      assert.strictEqual(identifier.transaction, expectedId);
+      assert.strictEqual(identifier.session, SESSION.id);
+      assert.strictEqual(identifier.timestamp, TIMESTAMP);
+    });
+  });
+});
