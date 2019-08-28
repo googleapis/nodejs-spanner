5d8a52f82b31023445383500946ae07927439075
Dave Gramlich <callmehiphop@gmail.com>
2019-08-28 15:55:33 -0400

    fix(types): import request types from teeny-request

--- src/session.ts
+++ src/session.ts
@@ -24,7 +24,7 @@ import {ServiceObject} from '@google-cloud/common-grpc';
 import {promisifyAll} from '@google-cloud/promisify';
 import * as extend from 'extend';
 import * as is from 'is';
-import * as r from 'request';
+import * as r from 'teeny-request';
 import {
   Snapshot,
   Transaction,
