895a599c42f70b3970d20f42f0fd367a7e47cf0b
alex <avaksman@gmail.com>
2020-06-19 01:09:14 -0400

    fix: race condition in should transfer value from one record to another using DML statements within a transaction test

--- samples/dml.js
+++ samples/dml.js
@@ -474,32 +474,46 @@ function writeWithTransactionUsingDml(instanceId, databaseId, projectId) {
         // Note: Cloud Spanner interprets Node.js numbers as FLOAT64s, so they
         // must be converted (back) to strings before being inserted as INT64s.
 
-        transaction.runUpdate({
-          sql: `UPDATE Albums SET MarketingBudget = @Budget
-            WHERE SingerId = 1 and AlbumId = 1`,
-          params: {
-            Budget: firstBudget.toString(),
-          },
-        });
-        transaction.runUpdate({
-          sql: `UPDATE Albums SET MarketingBudget = @Budget
-            WHERE SingerId = 2 and AlbumId = 2`,
-          params: {
-            Budget: secondBudget.toString(),
-          },
-        });
+        return transaction
+          .runUpdate({
+            sql: `UPDATE Albums SET MarketingBudget = @Budget
+                WHERE SingerId = 1 and AlbumId = 1`,
+            params: {
+              Budget: firstBudget,
+            },
+          })
+          .then(() =>
+            transaction.runUpdate({
+              sql: `UPDATE Albums SET MarketingBudget = @Budget
+                    WHERE SingerId = 2 and AlbumId = 2`,
+              params: {
+                Budget: secondBudget,
+              },
+            })
+          );
+      })
+      .then(() => {
+        // Get updated albums' budgets.
+        const query = `SELECT MarketingBudget FROM Albums
+          WHERE (SingerId = 1 AND AlbumId = 1) OR (SingerId = 2 AND AlbumId = 2)
+          ORDER BY SingerId`;
+        return transaction.run(query);
+      })
+      .then(results => {
+        // Verify the accuracy of the new budgets.
+        const rows = results[0].map(row => row.toJSON());
+        if (
+          rows[0].MarketingBudget === firstBudget &&
+          rows[1].MarketingBudget === secondBudget
+        ) {
+          console.log(
+            `Successfully executed read-write transaction using DML to transfer ${transferAmount} from Album 2 to Album 1.`
+          );
+        }
       })
       .then(() => {
         // Commits the transaction and send the changes to the database
         return transaction.commit();
-      })
-      .then(() => {
-        console.log(
-          `Successfully executed read-write transaction using DML to transfer ${transferAmount} from Album 2 to Album 1.`
-        );
-      })
-      .catch(err => {
-        console.error('ERROR:', err);
       })
       .then(() => {
         // Closes the database when finished
