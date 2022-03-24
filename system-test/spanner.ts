6112321273eca3a473312edf41b61a0b1d80428c
Astha Mohta <asthamohta@google.com>
2022-03-24 14:49:50 +0530

    adding data and commit timestamp

--- system-test/spanner.ts
+++ system-test/spanner.ts
@@ -36,6 +36,7 @@ import {grpc, CallOptions} from 'google-gax';
 import {google} from '../protos/protos';
 import CreateDatabaseMetadata = google.spanner.admin.database.v1.CreateDatabaseMetadata;
 import CreateBackupMetadata = google.spanner.admin.database.v1.CreateBackupMetadata;
+import {date} from 'is';
 
 const SKIP_BACKUPS = process.env.SKIP_BACKUPS;
 const PREFIX = 'gcloud-tests-';
@@ -43,7 +44,7 @@ const RUN_ID = shortUUID();
 const LABEL = `node-spanner-systests-${RUN_ID}`;
 const spanner = new Spanner({
   projectId: process.env.GCLOUD_PROJECT,
-  apiEndpoint: process.env.API_ENDPOINT,
+  apiEndpoint: 'staging-wrenchworks.sandbox.googleapis.com',
 });
 const GAX_OPTIONS: CallOptions = {
   retry: {
@@ -75,7 +76,7 @@ describe('Spanner', () => {
     : spanner.instance(generateName('instance'));
 
   const INSTANCE_CONFIG = {
-    config: 'regional-us-west2',
+    config: 'cloud-devel-global-config',
     nodes: 1,
     labels: {
       [LABEL]: 'true',
@@ -212,79 +213,81 @@ describe('Spanner', () => {
       });
     }
 
-    before(async () => {
+    
+    before(done => {
       if (IS_EMULATOR_ENABLED) {
-        const [googleSqlOperationUpdateDDL] = await DATABASE.updateSchema(
+        DATABASE.updateSchema(
           `
-              CREATE TABLE ${TABLE_NAME}
-              (
-                Key             STRING( MAX) NOT NULL,
-                BytesValue      BYTES( MAX),
-                BoolValue       BOOL,
-                DateValue       DATE,
-                FloatValue      FLOAT64,
-                IntValue        INT64,
-                NumericValue    NUMERIC,
-                StringValue     STRING( MAX),
-                TimestampValue  TIMESTAMP,
-                BytesArray      ARRAY<BYTES(MAX)>,
-                BoolArray       ARRAY<BOOL>,
-                DateArray       ARRAY< DATE >,
-                FloatArray      ARRAY<FLOAT64>,
-                IntArray        ARRAY<INT64>,
-                NumericArray    ARRAY< NUMERIC >,
-                StringArray     ARRAY<STRING(MAX)>,
-                TimestampArray  ARRAY< TIMESTAMP >,
-                CommitTimestamp TIMESTAMP OPTIONS (allow_commit_timestamp= true)
-              ) PRIMARY KEY (Key)
-            `
+            CREATE TABLE ${TABLE_NAME} (
+              Key STRING(MAX) NOT NULL,
+              BytesValue BYTES(MAX),
+              BoolValue BOOL,
+              DateValue DATE,
+              FloatValue FLOAT64,
+              IntValue INT64,
+              NumericValue NUMERIC,
+              StringValue STRING(MAX),
+              TimestampValue TIMESTAMP,
+              BytesArray ARRAY<BYTES(MAX)>,
+              BoolArray ARRAY<BOOL>,
+              DateArray ARRAY<DATE>,
+              FloatArray ARRAY<FLOAT64>,
+              IntArray ARRAY<INT64>,
+              NumericArray ARRAY<NUMERIC>,
+              StringArray ARRAY<STRING(MAX)>,
+              TimestampArray ARRAY<TIMESTAMP>,
+              CommitTimestamp TIMESTAMP OPTIONS (allow_commit_timestamp=true)
+            ) PRIMARY KEY (Key)
+            `,
+          execAfterOperationComplete(done)
         );
-        await googleSqlOperationUpdateDDL.promise();
       } else {
-        const [googleSqlOperationUpdateDDL] = await DATABASE.updateSchema(
+        const postgresCreateDatabase = () => {
+          PG_DATABASE.updateSchema(
+            `
+              CREATE TABLE ${TABLE_NAME} (
+                "Key" VARCHAR NOT NULL PRIMARY KEY,
+                "BytesValue" BYTEA,
+                "BoolValue" BOOL,
+                "FloatValue" DOUBLE PRECISION,
+                "IntValue" BIGINT,
+                "NumericValue" NUMERIC,
+                "StringValue" VARCHAR,
+                "TimestampValue" TIMESTAMPTZ,
+                "DateValue" DATE,
+                "CommitTimestamp" SPANNER.COMMIT_TIMESTAMP 
+              ); 
+            `,
+            execAfterOperationComplete(done)
+          );
+        };
+        DATABASE.updateSchema(
           `
-              CREATE TABLE ${TABLE_NAME}
-              (
-                Key             STRING( MAX) NOT NULL,
-                BytesValue      BYTES( MAX),
-                BoolValue       BOOL,
-                DateValue       DATE,
-                FloatValue      FLOAT64,
-                JsonValue       JSON,
-                IntValue        INT64,
-                NumericValue    NUMERIC,
-                StringValue     STRING( MAX),
-                TimestampValue  TIMESTAMP,
-                BytesArray      ARRAY<BYTES(MAX)>,
-                BoolArray       ARRAY<BOOL>,
-                DateArray       ARRAY< DATE >,
-                FloatArray      ARRAY<FLOAT64>,
-                JsonArray       ARRAY<JSON>,
-                IntArray        ARRAY<INT64>,
-                NumericArray    ARRAY< NUMERIC >,
-                StringArray     ARRAY<STRING(MAX)>,
-                TimestampArray  ARRAY< TIMESTAMP >,
-                CommitTimestamp TIMESTAMP OPTIONS (allow_commit_timestamp= true)
+              CREATE TABLE ${TABLE_NAME} (
+                Key STRING(MAX) NOT NULL,
+                BytesValue BYTES(MAX),
+                BoolValue BOOL,
+                DateValue DATE,
+                FloatValue FLOAT64,
+                JsonValue JSON,
+                IntValue INT64,
+                NumericValue NUMERIC,
+                StringValue STRING(MAX),
+                TimestampValue TIMESTAMP,
+                BytesArray ARRAY<BYTES(MAX)>,
+                BoolArray ARRAY<BOOL>,
+                DateArray ARRAY<DATE>,
+                FloatArray ARRAY<FLOAT64>,
+                JsonArray ARRAY<JSON>,
+                IntArray ARRAY<INT64>,
+                NumericArray ARRAY<NUMERIC>,
+                StringArray ARRAY<STRING(MAX)>,
+                TimestampArray ARRAY<TIMESTAMP>,
+                CommitTimestamp TIMESTAMP OPTIONS (allow_commit_timestamp=true)
               ) PRIMARY KEY (Key)
-            `
+            `,
+          postgresCreateDatabase
         );
-        await googleSqlOperationUpdateDDL.promise();
-        const [postgreSqlOperationUpdateDDL] = await PG_DATABASE.updateSchema(
-          `
-                CREATE TABLE ${TABLE_NAME}
-                (
-                  "Key"            VARCHAR NOT NULL PRIMARY KEY,
-                  "BytesValue"     BYTEA,
-                  "BoolValue"      BOOL,
-                  "FloatValue"     DOUBLE PRECISION,
-                  "IntValue"       BIGINT,
-                  "NumericValue"   NUMERIC,
-                  "StringValue"    VARCHAR,
-                  "TimestampValue" TIMESTAMPTZ
-                );
-            `
-        );
-        await postgreSqlOperationUpdateDDL.promise();
       }
     });
 
@@ -1113,22 +1116,42 @@ describe('Spanner', () => {
     });
 
     describe('dates', () => {
-      it('GOOGLE_STANDARD_SQL should write date values', done => {
-        const date = Spanner.date();
-
-        insert({DateValue: date}, Spanner.GOOGLE_STANDARD_SQL, (err, row) => {
+      const dateInsert = (done, dialect) => {
+        insert({DateValue: Spanner.date()}, dialect, (err, row) => {
           assert.ifError(err);
-          assert.deepStrictEqual(Spanner.date(row.toJSON().DateValue), date);
+          assert.deepStrictEqual(
+            Spanner.date(row.toJSON().DateValue),
+            Spanner.date()
+          );
           done();
         });
+      };
+
+      it('GOOGLE_STANDARD_SQL should write date values', done => {
+        dateInsert(done, Spanner.GOOGLE_STANDARD_SQL);
       });
 
-      it('GOOGLE_STANDARD_SQL should write null date values', done => {
-        insert({DateValue: null}, Spanner.GOOGLE_STANDARD_SQL, (err, row) => {
+      it('POSTGRESQL should write date values', function (done) {
+        if (IS_EMULATOR_ENABLED) {
+          this.skip();
+        }
+        dateInsert(done, Spanner.POSTGRESQL);
+      });
+
+      const dateInsertNull = (done, dialect) => {
+        insert({DateValue: null}, dialect, (err, row) => {
           assert.ifError(err);
           assert.strictEqual(row.toJSON().DateValue, null);
           done();
         });
+      };
+
+      it('GOOGLE_STANDARD_SQL should write null date values', done => {
+        dateInsertNull(done, Spanner.GOOGLE_STANDARD_SQL);
+      });
+
+      it('POSTGRESQL should write null date values', done => {
+        dateInsertNull(done, Spanner.POSTGRESQL);
       });
 
       it('GOOGLE_STANDARD_SQL should write empty date array values', done => {
@@ -1160,22 +1183,26 @@ describe('Spanner', () => {
     });
 
     describe('commit timestamp', () => {
-      it('GOOGLE_STANDARD_SQL should accept the commit timestamp placeholder', done => {
+      const commitTimestamp = (done, dialect) => {
         const data = {CommitTimestamp: Spanner.COMMIT_TIMESTAMP};
 
-        insert(
-          data,
-          Spanner.GOOGLE_STANDARD_SQL,
-          (err, row, {commitTimestamp}) => {
-            assert.ifError(err);
+        insert(data, dialect, (err, row, {commitTimestamp}) => {
+          assert.ifError(err);
 
-            const timestampFromCommit = Spanner.timestamp(commitTimestamp);
-            const timestampFromRead = row.toJSON().CommitTimestamp;
+          const timestampFromCommit = Spanner.timestamp(commitTimestamp);
+          const timestampFromRead = row.toJSON().CommitTimestamp;
 
-            assert.deepStrictEqual(timestampFromCommit, timestampFromRead);
-            done();
-          }
-        );
+          assert.deepStrictEqual(timestampFromCommit, timestampFromRead);
+          done();
+        });
+      };
+
+      it('GOOGLE_STANDARD_SQL should accept the commit timestamp placeholder', done => {
+        commitTimestamp(done, Spanner.GOOGLE_STANDARD_SQL);
+      });
+
+      it('POSTGRESQL should accept the commit timestamp placeholder', done => {
+        commitTimestamp(done, Spanner.POSTGRESQL);
       });
     });
 
@@ -3861,24 +3888,42 @@ describe('Spanner', () => {
         });
 
         describe('date', () => {
+          const dateQuery = (done, database, query, value) => {
+            database.run(query, (err, rows) => {
+              assert.ifError(err);
+
+              let returnedDate = Spanner.date(rows[0][0].value);
+              if(value === null) {
+                returnedDate = rows[0][0].value;
+              }
+              assert.deepStrictEqual(returnedDate, value);
+              done();
+            });
+          };
+
           it('GOOGLE_STANDARD_SQL should bind the value', done => {
             const date = Spanner.date();
-
             const query = {
               sql: 'SELECT @v',
               params: {
                 v: date,
               },
             };
+            dateQuery(done, DATABASE, query, date);
+          });
 
-            DATABASE.run(query, (err, rows) => {
-              assert.ifError(err);
-
-              const returnedDate = Spanner.date(rows[0][0].value);
-              assert.deepStrictEqual(returnedDate, date);
-
-              done();
-            });
+          it('POSTGRESQL should bind the value', function (done) {
+            if (IS_EMULATOR_ENABLED) {
+              this.skip();
+            }
+            const date = Spanner.date();
+            const query = {
+              sql: 'SELECT $1',
+              params: {
+                p1: date,
+              },
+            };
+            dateQuery(done, PG_DATABASE, query, date);
           });
 
           it('GOOGLE_STANDARD_SQL should allow for null values', done => {
@@ -3891,12 +3936,23 @@ describe('Spanner', () => {
                 v: 'date',
               },
             };
+            dateQuery(done, DATABASE, query, null);
+          });
 
-            DATABASE.run(query, (err, rows) => {
-              assert.ifError(err);
-              assert.strictEqual(rows[0][0].value, null);
-              done();
-            });
+          it('POSTGRESQL should allow for null values', function (done) {
+            if (IS_EMULATOR_ENABLED) {
+              this.skip();
+            }
+            const query = {
+              sql: 'SELECT $1',
+              params: {
+                p1: null,
+              },
+              types: {
+                p1: 'date',
+              },
+            };
+            dateQuery(done, PG_DATABASE, query, null);
           });
 
           it('GOOGLE_STANDARD_SQL should bind arrays', done => {
