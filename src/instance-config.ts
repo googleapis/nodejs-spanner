bb1442c3a049c29ce97249931aae2f1937074b89
Rajat Bhatta <rajatrb@google.com>
2022-09-21 08:48:27 +0000

    fix: err object type

--- src/instance-config.ts
+++ src/instance-config.ts
@@ -459,7 +459,7 @@ class InstanceConfig extends common.GrpcServiceObject {
       // Found, therefore it exists
       return true;
     } catch (err) {
-      if (err.code === grpc.status.NOT_FOUND) {
+      if ((err as grpc.ServerErrorResponse).code === grpc.status.NOT_FOUND) {
         return false;
       }
       // Some other error occurred, rethrow
