cac2ee2fcd6ed691e417349dc65df448f89ac77c
Sri Harsha CH <sriharshach@google.com>
2024-05-21 14:21:38 +0000

    chore(spanner): regenerate proto files

--- test/data/singer.js
+++ test/data/singer.js
@@ -13,19 +13,18 @@
 // limitations under the License.
 
 /*eslint-disable block-scoped-var, id-length, no-control-regex, no-magic-numbers, no-prototype-builtins, no-redeclare, no-shadow, no-var, sort-vars*/
-'use strict';
+"use strict";
 
-var $protobuf = require('protobufjs/minimal');
+var $protobuf = require("protobufjs/minimal");
 
 // Common aliases
-var $Reader = $protobuf.Reader,
-  $Writer = $protobuf.Writer,
-  $util = $protobuf.util;
+var $Reader = $protobuf.Reader, $Writer = $protobuf.Writer, $util = $protobuf.util;
 
 // Exported root namespace
-var $root = $protobuf.roots['default'] || ($protobuf.roots['default'] = {});
+var $root = $protobuf.roots["default"] || ($protobuf.roots["default"] = {});
 
-$root.examples = (function () {
+$root.examples = (function() {
+
   /**
    * Namespace examples.
    * @exports examples
@@ -33,7 +32,8 @@ $root.examples = (function () {
    */
   var examples = {};
 
-  examples.spanner = (function () {
+  examples.spanner = (function() {
+
     /**
      * Namespace spanner.
      * @memberof examples
@@ -41,7 +41,8 @@ $root.examples = (function () {
      */
     var spanner = {};
 
-    spanner.music = (function () {
+    spanner.music = (function() {
+
       /**
        * Namespace music.
        * @memberof examples.spanner
@@ -49,7 +50,8 @@ $root.examples = (function () {
        */
       var music = {};
 
-      music.SingerInfo = (function () {
+      music.SingerInfo = (function() {
+
         /**
          * Properties of a SingerInfo.
          * @memberof examples.spanner.music
@@ -70,48 +72,89 @@ $root.examples = (function () {
          */
         function SingerInfo(properties) {
           if (properties)
-            for (
-              var keys = Object.keys(properties), i = 0;
-              i < keys.length;
-              ++i
-            )
-              if (properties[keys[i]] !== null)
+            for (var keys = Object.keys(properties), i = 0; i < keys.length; ++i)
+              if (properties[keys[i]] != null)
                 this[keys[i]] = properties[keys[i]];
         }
 
         /**
          * SingerInfo singerId.
-         * @member {number|Long} singerId
+         * @member {number|Long|null|undefined} singerId
          * @memberof examples.spanner.music.SingerInfo
          * @instance
          */
-        SingerInfo.prototype.singerId = $util.Long
-          ? $util.Long.fromBits(0, 0, false)
-          : 0;
+        SingerInfo.prototype.singerId = null;
 
         /**
          * SingerInfo birthDate.
-         * @member {string} birthDate
+         * @member {string|null|undefined} birthDate
          * @memberof examples.spanner.music.SingerInfo
          * @instance
          */
-        SingerInfo.prototype.birthDate = '';
+        SingerInfo.prototype.birthDate = null;
 
         /**
          * SingerInfo nationality.
-         * @member {string} nationality
+         * @member {string|null|undefined} nationality
          * @memberof examples.spanner.music.SingerInfo
          * @instance
          */
-        SingerInfo.prototype.nationality = '';
+        SingerInfo.prototype.nationality = null;
 
         /**
          * SingerInfo genre.
-         * @member {examples.spanner.music.Genre} genre
+         * @member {examples.spanner.music.Genre|null|undefined} genre
          * @memberof examples.spanner.music.SingerInfo
          * @instance
          */
-        SingerInfo.prototype.genre = 0;
+        SingerInfo.prototype.genre = null;
+
+        // OneOf field names bound to virtual getters and setters
+        var $oneOfFields;
+
+        /**
+         * SingerInfo _singerId.
+         * @member {"singerId"|undefined} _singerId
+         * @memberof examples.spanner.music.SingerInfo
+         * @instance
+         */
+        Object.defineProperty(SingerInfo.prototype, "_singerId", {
+          get: $util.oneOfGetter($oneOfFields = ["singerId"]),
+          set: $util.oneOfSetter($oneOfFields)
+        });
+
+        /**
+         * SingerInfo _birthDate.
+         * @member {"birthDate"|undefined} _birthDate
+         * @memberof examples.spanner.music.SingerInfo
+         * @instance
+         */
+        Object.defineProperty(SingerInfo.prototype, "_birthDate", {
+          get: $util.oneOfGetter($oneOfFields = ["birthDate"]),
+          set: $util.oneOfSetter($oneOfFields)
+        });
+
+        /**
+         * SingerInfo _nationality.
+         * @member {"nationality"|undefined} _nationality
+         * @memberof examples.spanner.music.SingerInfo
+         * @instance
+         */
+        Object.defineProperty(SingerInfo.prototype, "_nationality", {
+          get: $util.oneOfGetter($oneOfFields = ["nationality"]),
+          set: $util.oneOfSetter($oneOfFields)
+        });
+
+        /**
+         * SingerInfo _genre.
+         * @member {"genre"|undefined} _genre
+         * @memberof examples.spanner.music.SingerInfo
+         * @instance
+         */
+        Object.defineProperty(SingerInfo.prototype, "_genre", {
+          get: $util.oneOfGetter($oneOfFields = ["genre"]),
+          set: $util.oneOfSetter($oneOfFields)
+        });
 
         /**
          * Creates a new SingerInfo instance using the specified properties.
@@ -135,29 +178,16 @@ $root.examples = (function () {
          * @returns {$protobuf.Writer} Writer
          */
         SingerInfo.encode = function encode(message, writer) {
-          if (!writer) writer = $Writer.create();
-          if (
-            message.singerId !== null &&
-            Object.hasOwnProperty.call(message, 'singerId')
-          )
-            writer.uint32(/* id 1, wireType 0 =*/ 8).int64(message.singerId);
-          if (
-            message.birthDate !== null &&
-            Object.hasOwnProperty.call(message, 'birthDate')
-          )
-            writer.uint32(/* id 2, wireType 2 =*/ 18).string(message.birthDate);
-          if (
-            message.nationality !== null &&
-            Object.hasOwnProperty.call(message, 'nationality')
-          )
-            writer
-              .uint32(/* id 3, wireType 2 =*/ 26)
-              .string(message.nationality);
-          if (
-            message.genre !== null &&
-            Object.hasOwnProperty.call(message, 'genre')
-          )
-            writer.uint32(/* id 4, wireType 0 =*/ 32).int32(message.genre);
+          if (!writer)
+            writer = $Writer.create();
+          if (message.singerId != null && Object.hasOwnProperty.call(message, "singerId"))
+            writer.uint32(/* id 1, wireType 0 =*/8).int64(message.singerId);
+          if (message.birthDate != null && Object.hasOwnProperty.call(message, "birthDate"))
+            writer.uint32(/* id 2, wireType 2 =*/18).string(message.birthDate);
+          if (message.nationality != null && Object.hasOwnProperty.call(message, "nationality"))
+            writer.uint32(/* id 3, wireType 2 =*/26).string(message.nationality);
+          if (message.genre != null && Object.hasOwnProperty.call(message, "genre"))
+            writer.uint32(/* id 4, wireType 0 =*/32).int32(message.genre);
           return writer;
         };
 
@@ -186,9 +216,9 @@ $root.examples = (function () {
          * @throws {$protobuf.util.ProtocolError} If required fields are missing
          */
         SingerInfo.decode = function decode(reader, length) {
-          if (!(reader instanceof $Reader)) reader = $Reader.create(reader);
-          var end = length === undefined ? reader.len : reader.pos + length,
-            message = new $root.examples.spanner.music.SingerInfo();
+          if (!(reader instanceof $Reader))
+            reader = $Reader.create(reader);
+          var end = length === undefined ? reader.len : reader.pos + length, message = new $root.examples.spanner.music.SingerInfo();
           while (reader.pos < end) {
             var tag = reader.uint32();
             switch (tag >>> 3) {
@@ -227,7 +257,8 @@ $root.examples = (function () {
          * @throws {$protobuf.util.ProtocolError} If required fields are missing
          */
         SingerInfo.decodeDelimited = function decodeDelimited(reader) {
-          if (!(reader instanceof $Reader)) reader = new $Reader(reader);
+          if (!(reader instanceof $Reader))
+            reader = new $Reader(reader);
           return this.decode(reader, reader.uint32());
         };
 
@@ -240,37 +271,36 @@ $root.examples = (function () {
          * @returns {string|null} `null` if valid, otherwise the reason why it is not
          */
         SingerInfo.verify = function verify(message) {
-          if (typeof message !== 'object' || message === null)
-            return 'object expected';
-          if (message.singerId !== null && message.hasOwnProperty('singerId'))
-            if (
-              !$util.isInteger(message.singerId) &&
-              !(
-                message.singerId &&
-                $util.isInteger(message.singerId.low) &&
-                $util.isInteger(message.singerId.high)
-              )
-            )
-              return 'singerId: integer|Long expected';
-          if (message.birthDate !== null && message.hasOwnProperty('birthDate'))
+          if (typeof message !== "object" || message === null)
+            return "object expected";
+          var properties = {};
+          if (message.singerId != null && message.hasOwnProperty("singerId")) {
+            properties._singerId = 1;
+            if (!$util.isInteger(message.singerId) && !(message.singerId && $util.isInteger(message.singerId.low) && $util.isInteger(message.singerId.high)))
+              return "singerId: integer|Long expected";
+          }
+          if (message.birthDate != null && message.hasOwnProperty("birthDate")) {
+            properties._birthDate = 1;
             if (!$util.isString(message.birthDate))
-              return 'birthDate: string expected';
-          if (
-            message.nationality !== null &&
-            message.hasOwnProperty('nationality')
-          )
+              return "birthDate: string expected";
+          }
+          if (message.nationality != null && message.hasOwnProperty("nationality")) {
+            properties._nationality = 1;
             if (!$util.isString(message.nationality))
-              return 'nationality: string expected';
-          if (message.genre !== null && message.hasOwnProperty('genre'))
+              return "nationality: string expected";
+          }
+          if (message.genre != null && message.hasOwnProperty("genre")) {
+            properties._genre = 1;
             switch (message.genre) {
               default:
-                return 'genre: enum value expected';
+                return "genre: enum value expected";
               case 0:
               case 1:
               case 2:
               case 3:
                 break;
             }
+          }
           return null;
         };
 
@@ -286,44 +316,39 @@ $root.examples = (function () {
           if (object instanceof $root.examples.spanner.music.SingerInfo)
             return object;
           var message = new $root.examples.spanner.music.SingerInfo();
-          if (object.singerId !== null)
+          if (object.singerId != null)
             if ($util.Long)
-              (message.singerId = $util.Long.fromValue(
-                object.singerId
-              )).unsigned = false;
-            else if (typeof object.singerId === 'string')
+              (message.singerId = $util.Long.fromValue(object.singerId)).unsigned = false;
+            else if (typeof object.singerId === "string")
               message.singerId = parseInt(object.singerId, 10);
-            else if (typeof object.singerId === 'number')
+            else if (typeof object.singerId === "number")
               message.singerId = object.singerId;
-            else if (typeof object.singerId === 'object')
-              message.singerId = new $util.LongBits(
-                object.singerId.low >>> 0,
-                object.singerId.high >>> 0
-              ).toNumber();
-          if (object.birthDate !== null)
+            else if (typeof object.singerId === "object")
+              message.singerId = new $util.LongBits(object.singerId.low >>> 0, object.singerId.high >>> 0).toNumber();
+          if (object.birthDate != null)
             message.birthDate = String(object.birthDate);
-          if (object.nationality !== null)
+          if (object.nationality != null)
             message.nationality = String(object.nationality);
           switch (object.genre) {
             default:
-              if (typeof object.genre === 'number') {
+              if (typeof object.genre === "number") {
                 message.genre = object.genre;
                 break;
               }
               break;
-            case 'POP':
+            case "POP":
             case 0:
               message.genre = 0;
               break;
-            case 'JAZZ':
+            case "JAZZ":
             case 1:
               message.genre = 1;
               break;
-            case 'FOLK':
+            case "FOLK":
             case 2:
               message.genre = 2;
               break;
-            case 'ROCK':
+            case "ROCK":
             case 3:
               message.genre = 3;
               break;
@@ -341,53 +366,32 @@ $root.examples = (function () {
          * @returns {Object.<string,*>} Plain object
          */
         SingerInfo.toObject = function toObject(message, options) {
-          if (!options) options = {};
+          if (!options)
+            options = {};
           var object = {};
-          if (options.defaults) {
-            if ($util.Long) {
-              var long = new $util.Long(0, 0, false);
-              object.singerId =
-                options.longs === String
-                  ? long.toString()
-                  : options.longs === Number
-                    ? long.toNumber()
-                    : long;
-            } else object.singerId = options.longs === String ? '0' : 0;
-            object.birthDate = '';
-            object.nationality = '';
-            object.genre = options.enums === String ? 'POP' : 0;
-          }
-          if (message.singerId !== null && message.hasOwnProperty('singerId'))
-            if (typeof message.singerId === 'number')
-              object.singerId =
-                options.longs === String
-                  ? String(message.singerId)
-                  : message.singerId;
+          if (message.singerId != null && message.hasOwnProperty("singerId")) {
+            if (typeof message.singerId === "number")
+              object.singerId = options.longs === String ? String(message.singerId) : message.singerId;
             else
-              object.singerId =
-                options.longs === String
-                  ? $util.Long.prototype.toString.call(message.singerId)
-                  : options.longs === Number
-                    ? new $util.LongBits(
-                        message.singerId.low >>> 0,
-                        message.singerId.high >>> 0
-                      ).toNumber()
-                    : message.singerId;
-          if (message.birthDate !== null && message.hasOwnProperty('birthDate'))
+              object.singerId = options.longs === String ? $util.Long.prototype.toString.call(message.singerId) : options.longs === Number ? new $util.LongBits(message.singerId.low >>> 0, message.singerId.high >>> 0).toNumber() : message.singerId;
+            if (options.oneofs)
+              object._singerId = "singerId";
+          }
+          if (message.birthDate != null && message.hasOwnProperty("birthDate")) {
             object.birthDate = message.birthDate;
-          if (
-            message.nationality !== null &&
-            message.hasOwnProperty('nationality')
-          )
+            if (options.oneofs)
+              object._birthDate = "birthDate";
+          }
+          if (message.nationality != null && message.hasOwnProperty("nationality")) {
             object.nationality = message.nationality;
-          if (message.genre !== null && message.hasOwnProperty('genre'))
-            object.genre =
-              options.enums === String
-                ? $root.examples.spanner.music.Genre[message.genre] ===
-                  undefined
-                  ? message.genre
-                  : $root.examples.spanner.music.Genre[message.genre]
-                : message.genre;
+            if (options.oneofs)
+              object._nationality = "nationality";
+          }
+          if (message.genre != null && message.hasOwnProperty("genre")) {
+            object.genre = options.enums === String ? $root.examples.spanner.music.Genre[message.genre] === undefined ? message.genre : $root.examples.spanner.music.Genre[message.genre] : message.genre;
+            if (options.oneofs)
+              object._genre = "genre";
+          }
           return object;
         };
 
@@ -412,9 +416,9 @@ $root.examples = (function () {
          */
         SingerInfo.getTypeUrl = function getTypeUrl(typeUrlPrefix) {
           if (typeUrlPrefix === undefined) {
-            typeUrlPrefix = 'type.googleapis.com';
+            typeUrlPrefix = "type.googleapis.com";
           }
-          return typeUrlPrefix + '/examples.spanner.music.SingerInfo';
+          return typeUrlPrefix + "/examples.spanner.music.SingerInfo";
         };
 
         return SingerInfo;
@@ -429,13 +433,12 @@ $root.examples = (function () {
        * @property {number} FOLK=2 FOLK value
        * @property {number} ROCK=3 ROCK value
        */
-      music.Genre = (function () {
-        var valuesById = {},
-          values = Object.create(valuesById);
-        values[(valuesById[0] = 'POP')] = 0;
-        values[(valuesById[1] = 'JAZZ')] = 1;
-        values[(valuesById[2] = 'FOLK')] = 2;
-        values[(valuesById[3] = 'ROCK')] = 3;
+      music.Genre = (function() {
+        var valuesById = {}, values = Object.create(valuesById);
+        values[valuesById[0] = "POP"] = 0;
+        values[valuesById[1] = "JAZZ"] = 1;
+        values[valuesById[2] = "FOLK"] = 2;
+        values[valuesById[3] = "ROCK"] = 3;
         return values;
       })();
 
