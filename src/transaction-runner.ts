d57425285207bc3ddee339e65d693c0acbe652c1
Alka Trivedi <alkatrivedi12dec@gmail.com>
2025-03-06 13:07:17 +0530

    feat(spanner): add support for snapshot isolation

--- src/transaction-runner.ts
+++ src/transaction-runner.ts
@@ -26,6 +26,7 @@ import {isSessionNotFoundError} from './session-pool';
 import {Database} from './database';
 import {google} from '../protos/protos';
 import IRequestOptions = google.spanner.v1.IRequestOptions;
+import {protos} from '.';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const jsonProtos = require('../protos/protos.json');
@@ -46,6 +47,7 @@ export interface RunTransactionOptions {
   requestOptions?: Pick<IRequestOptions, 'transactionTag'>;
   optimisticLock?: boolean;
   excludeTxnFromChangeStreams?: boolean;
+  isolationLevel?: protos.google.spanner.v1.TransactionOptions.IsolationLevel;
 }
 
 /**
