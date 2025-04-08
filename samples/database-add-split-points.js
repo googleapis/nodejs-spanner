1f26ca8433c322fa1c7b5d8c3f9e8b6e6abb84a5
Akshara Uke <aksharau@google.com>
2025-04-08 13:18:35 +0000

    more lint fixes

--- samples/database-add-split-points.js
+++ samples/database-add-split-points.js
@@ -51,13 +51,12 @@ function main(
           },
           {
             index: 'SingersByFirstLastName',
-            keys: [ {keyParts: {values: ['Jane', 'Doe']}} ],
+            keys: [{keyParts: {values: ['Jane', 'Doe']}} ],
           },
           {
             index: 'SingersByFirstLastName',
-            keys: [ {keyParts: {values: ['38']}},{keyParts: {values: ['John', 'Doe']}}],
-          },
-                  ],
+            keys: [{keyParts: {values: ['38']}},{keyParts: {values: ['John', 'Doe']}}],
+          },],
       });
       console.log('Added Split Points:', response);
     } catch (err) {
