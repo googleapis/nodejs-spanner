2fa395a2e5101b0fce2e0a3319fe4779fdd99e8e
Akshara Uke <aksharau@google.com>
2025-04-08 13:09:36 +0000

    more lint fixes

--- samples/database-add-split-points.js
+++ samples/database-add-split-points.js
@@ -13,83 +13,64 @@
  * limitations under the License.
  */
 
-//  sample-metadata:
+// sample-metadata:
 //  title: Adds split points to a database.
 //  usage: node database-add-split-points.js <PROJECT_ID> <INSTANCE_ID> <DATABASE_ID>
 
 'use strict';
 
 function main(
-    projectId = 'my-project-id',
-    instanceId = 'my-instance-id',
-    databaseId = 'my-database-id'
+  projectId = 'my-project-id',
+  instanceId = 'my-instance-id',
+  databaseId = 'my-database-id'
 ) {
-    async function addSplitPoints() {
-        // [START spanner_database_add_split_points]
-        // Import the Google Cloud client library for Spanner.
-        const {
-            Spanner
-        } = require('@google-cloud/spanner');
+  async function addSplitPoints() {
+    // [START spanner_database_add_split_points]
+    // Import the Google Cloud client library for Spanner.
+    const {Spanner} = require('@google-cloud/spanner');
 
-        /**
-         * TODO(developer): Uncomment these variables before running the sample.
-         */
-        // const projectId = 'my-project-id';
-        // const instanceId = 'my-instance-id';
-        // const databaseId = 'my-database-id';
+    /**
+     * TODO(developer): Uncomment these variables before running the sample.
+     */
+    // const projectId = 'my-project-id';
+    // const instanceId = 'my-instance-id';
+    // const databaseId = 'my-database-id';
 
-        // Create a Spanner database admin client.
-        const spanner = new Spanner({
-            projectId
-        });
-        const client = spanner.getDatabaseAdminClient();
+    // Create a Spanner database admin client.
+    const spanner = new Spanner({projectId});
+    const client = spanner.getDatabaseAdminClient();
 
-        try {
-            // Add split points to table and index
-            const [response] = await client.addSplitPoints({
-                database: client.databasePath(projectId, instanceId, databaseId),
-                splitPoints: [{
-                        table: 'Singers',
-                        keys: [{
-                            keyParts: {
-                                values: ['42']
-                            }
-                        }],
-                    },
-                    {
-                        index: 'SingersByFirstLastName',
-                        keys: [{
-                            keyParts: {
-                                values: ['Jane', 'Doe']
-                            }
-                        }],
-                    },
-                    {
-                        index: 'SingersByFirstLastName',
-                        keys: [{
-                            keyParts: {
-                                values: ['38']
-                            }
-                        }, {
-                            keyParts: {
-                                values: ['John', 'Doe']
-                            }
-                        }],
-                    },
-                ],
-            });
-            console.log('Added Split Points:', response);
-        } catch (err) {
-            console.error('ERROR:', err);
-        }
-        // [END spanner_database_add_split_points]
+    try {
+      // Add split points to table and index
+      const [response] = await client.addSplitPoints({
+        database: client.databasePath(projectId, instanceId, databaseId),
+        splitPoints: [
+          {
+            table: 'Singers',
+            keys: [{keyParts: {values: ['42']}}],
+          },
+          {
+            index: 'SingersByFirstLastName',
+            keys: [ {keyParts: {values: ['Jane', 'Doe']}} ],
+          },
+          {
+            index: 'SingersByFirstLastName',
+            keys: [ {keyParts: {values: ['38']}},{keyParts: {values: ['John', 'Doe']}}],
+          },
+                  ],
+      });
+      console.log('Added Split Points:', response);
+    } catch (err) {
+      console.error('ERROR:', err);
     }
+    // [END spanner_database_add_split_points]
+  }
 
-    addSplitPoints();
+  addSplitPoints();
 }
 
 process.on('unhandledRejection', err => {
-    console.error(err.message);
-    process.exitCode = 1;
+  console.error(err.message);
+  process.exitCode = 1;
 });
 main(...process.argv.slice(2));
